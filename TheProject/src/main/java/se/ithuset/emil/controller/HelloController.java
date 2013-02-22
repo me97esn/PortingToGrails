5522eccc7619b83d47a0bdcab20058dd8a6e884b
Emil Stenberg <esandin@gmail.com>
2013-02-22 10:55:43 +0100

    Using spring mvc for hello world html

---
+++ TheProject/src/main/java/se/ithuset/emil/controller/HelloController.java
@@ -0,0 +1,20 @@
+package se.ithuset.emil.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/welcome")
+public class HelloController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+        System.out.println("At least were accessing the controller");
+		model.addAttribute("message", "Spring 3 MVC Hello World");
+		return "hello";
+
+	}
+
+}
